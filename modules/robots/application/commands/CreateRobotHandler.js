2a632963685c430335979358d950bf3fd4e079d2
ulises <oscarulises1124@hotmail.com>
2022-04-27 22:06:57 -0500

    agregar comentarios de explicacion

--- modules/robots/application/commands/CreateRobotHandler.js
+++ modules/robots/application/commands/CreateRobotHandler.js
@@ -3,15 +3,15 @@ import { RobotRepository } from "../../infraestructure/repositories/RobotReposit
 
 export class CreateRobotHandler {
     constructor() {
+        /* nos traemos la funcionalidad de la clase RobotRepository */
         this.robotRepository = new RobotRepository();
     }
 
     async handle(command) {
-        /* Crear un nuevo robot y mandarselo al backend */
-
+        /* Crear un nuevo robot a partir de los datos del formulario */
         const robot = Factory.create({ id: command.id, name: command.name, model: command.model });
 
+        /* y lo almacenamos en el backend en esta instruccion */
         await this.robotRepository.createRobot(robot);
     }
-
 }
--- modules/robots/infraestructure/repositories/RobotRepository.js
+++ modules/robots/infraestructure/repositories/RobotRepository.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
 export class RobotRepository {
-    async createRobot(robot) {
+    async /* permite ejecutar funcionalidades que necesitan esperar */ createRobot(robot) {
+        /* ejecutamos la funcion toProperties para acceder a sus propiedades */
         const props = robot.toProperties();
+
         /* Axios es un cliente http que sirve para conectar con el backend */
-        await axios.post('http://localhost:3001/robots', {
+        await /* esperar */ axios.post('http://localhost:3001/robots', {
             id: props.id,
             name: props.name,
             model: props.model
