ee13fcc0c44b5638143a389fb4b51f8b80123180
ulises <oscarulises1124@hotmail.com>
2022-05-04 21:50:36 -0500

    ejecutar map del componente robot card

--- pages/robots/index.js
+++ pages/robots/index.js
@@ -1,27 +1,37 @@
 import { useState, useEffect } from 'react';
 import { RobotsPresenter } from '../../modules/robots/interface/RobotsPresenter';
+import RobotCard from '../../components/RobotCard';
 
 const Robots = () => {
     const robotsPresenter = new RobotsPresenter();
     const [robots, setRobots] = useState([]);
 
-    // Este hook o "metodo" se ejecuta al entar al componente 
+    // Este hook o "metodo" se ejecuta al entar al componente ya renderizado por primera vez
     useEffect(() => {
         const fetch = async () => {
             // ejecutamos el metodo listRobots que creamos en RobotsPresenter para listar los robots
             const result = await robotsPresenter.listRobots();
-            console.log('ahora si estamos en iterface grafica', result);
             setRobots(result);
         }
         fetch();
     }, []);
+
+    
+
     return (
         <>
             <h1>Robots funcionando</h1>
-            <ul>
-                <li>id {robots[0]?.id}</li>
-                <li>nombre {robots[0]?.name}</li>
-            </ul>
+
+            <div className="robot-cards-container">
+
+                {robots.map((robot, index) => {
+                    return (
+                        <RobotCard key={index} robot={robot} />
+                    )
+                })}
+
+            </div>
+
         </>
     );
 }
