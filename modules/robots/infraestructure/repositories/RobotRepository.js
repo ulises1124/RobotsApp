6aabb618f387afb5c51f49735c5cb64516e4381a
ulises <oscarulises1124@hotmail.com>
2022-05-04 19:25:23 -0500

    traer lista de robots desde el backend

--- modules/robots/infraestructure/repositories/RobotRepository.js
+++ modules/robots/infraestructure/repositories/RobotRepository.js
@@ -1,15 +1,30 @@
 import axios from "axios";
+import { Factory } from "../../domain/factory";
 
 export class RobotRepository {
-    async /* permite ejecutar funcionalidades que necesitan esperar */ createRobot(robot) {
-        /* ejecutamos la funcion toProperties para acceder a sus propiedades */
+    // ASYNC permite ejecutar funcionalidades que necesitan esperar
+    async createRobot(robot) {
+        // ejecutamos la funcion toProperties para acceder a sus propiedades
         const props = robot.toProperties();
 
-        /* Axios es un cliente http que sirve para conectar con el backend */
-        await /* esperar */ axios.post('http://localhost:3001/robots', {
+        // Axios es un cliente http que sirve para conectar con el backend
+        // AWAIT esperar
+        await axios.post('http://localhost:3001/robots', {
             id: props.id,
             name: props.name,
             model: props.model
         });
+    }
+    async listRobots() {
+        const httpResult = await axios.get('http://localhost:3001/robots');
+
+        const results = httpResult.data.robots;
+        
+        return results.map((result) => { return this.resultToRobot(result); });
+
+    }
+
+    resultToRobot(result) {
+        return Factory.reconstitute({ ...result });
     }
 }
